ba96f96206f77c8ea551bc2257c4535d8f527af0
Javier Peces <javierpeces@users.noreply.github.com>
2016-10-05 09:42:50 +0200

    Update and rename adobefondl.sh to adobe-font-dl-1.sh

--- adobe-font-dl-1.sh
+++ adobe-font-dl-1.sh
@@ -11,7 +11,7 @@
 raitnau=$(date +%F\ %T)
 myself=$(basename $0 .sh)
 username="your valid github.com username"
-password="your passvoid"
+password="yo passvoid"
 echo "${myself}: installing adobe fonts starts ${raitnau}..."
 
 declare -a fontname
