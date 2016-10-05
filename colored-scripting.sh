3a77f2d569f14aadad6a9285e53cb3c9a3055d4a
Javier Peces <javierpeces@users.noreply.github.com>
2016-10-05 10:43:40 +0200

    Create colored-scripting.sh

---
+++ colored-scripting.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#
+# color the world, one ping at a time
+#
+
+domain="vsphere.local"
+redtext="\e[00;31m"
+stdtext="\e[00m"
+hostlist="proxy nsdir rep01 web01 app09 alf01 alf02 sql01 sql02"
+
+for host in ${hostlist}
+do
+        rc=`ping -c1 ${host}.${domain} >/dev/null 2>&1; echo $?`
+        test "${rc}" -eq "0"  &&  text="[ OK ]"  ||  text="${redtext}[ KO ]${stdtext}"
+        echo -e "${host} ......: ${text}"
+done
